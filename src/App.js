cc09c92f634ce51cd5d699f124509af3955730ea
Jake Duncan <jakecduncan@gmail.com>
2021-05-24 22:09:59 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@material-ui/core";
+import styled from "styled-components";
+import "./index.css";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Typography variant="h2">Open Source Blog Using NextJS</Typography>
+      <Link href="https://demo.opensourceblog.dev">
+        Check it out <ArrowRightAltIcon />
+      </Link>
+    </AppContainer>
   );
 }
+
+const Link = styled.a`
+  color: #daa520;
+  font-size: x-large;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  margin-top: 10px;
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  margin-top: 10%;
+`;
 
 export default App;
